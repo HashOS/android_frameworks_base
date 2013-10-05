356e0b10e25f2e3065dfe119f1d0de8a3fb1e0fc
maxwen <max.weninger@gmail.com>
2013-10-05 13:15:36 +0200

    [1/3] base: add support for adb over network
    Change-Id: I4d76ffe5d502ddb21aacea353d7190be008cb50f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12170,6 +12170,12 @@ public final class Settings {
                 "max_sound_trigger_detection_service_ops_per_day";
 
         /**
+         * The TCP/IP port to run ADB on, or 0 for USB
+         * @hide
+         */
+        public static final String HASH_ADB_PORT = "hash_adb_port";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
