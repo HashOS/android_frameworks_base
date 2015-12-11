431bbf63cc8c8eb5543cd0403afeba987fd06e80
Pat Erley <perley@cyngn.com>
2015-12-10 17:00:53 -0800

    base: Log when proximity blocks wake
    There is currently no indication when the proximity sensor
    is allowed to prevent the system from waking up.  This can
    make it hard to debug weird failed wakes.  Add a log to
    facilitate debugging wakup problems.
    
    Change-Id: I7df34e13b1268a4e7211fb06ccd88f2053c0af0a

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -5093,6 +5093,8 @@ public final class PowerManagerService extends SystemService
                     if (distance >= PROXIMITY_NEAR_THRESHOLD ||
                             distance >= mProximitySensor.getMaximumRange()) {
                         r.run();
+                    } else {
+                        Slog.w(TAG, "Not waking up. Proximity sensor is blocked.");
                     }
                 }
 
