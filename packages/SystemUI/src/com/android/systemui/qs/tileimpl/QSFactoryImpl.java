551bea43c8d6c853f8de2de67d4ade1ecc520cd0
bigrushdog <randall.rushing@gmail.com>
2018-02-21 16:16:21 -0800

    base: Introduce Theme tile
    Long press tile to change between Style (dark/light/auto ui)
    mode and Accent (#FruityPebbles) mode.
    Click to show detail view.
    
    aex edits:- Include support for black and extended UI themes
                Update for Pie (@ishubhamsingh)
    
    Change-Id: Ic9ca2a3305b4b79120d7ffca607477636fbbb5c5

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.NavbarDisableTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ThemeTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WeatherTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -120,6 +121,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new KeyDisableTile(mHost);
             case "navbardisable":
                 return new NavbarDisableTile(mHost);
+            case "theme":
+                return new ThemeTile(mHost);
         }
 
         // Intent tiles.
