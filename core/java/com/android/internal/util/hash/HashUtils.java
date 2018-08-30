ed45b122590f43afcd667e29e3ba51277c537bdf
romanbb <romanbirg@gmail.com>
2018-08-30 11:04:47 +0200

    base: add isPackageInstalled boolean
    Change-Id: Ib23bdb4cf2f8f0ed909f52ac2dab6e18fe9a91d4

--- core/java/com/android/internal/util/hash/HashUtils.java
+++ core/java/com/android/internal/util/hash/HashUtils.java
@@ -32,6 +32,9 @@ import android.view.KeyEvent;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+
 public class HashUtils {
     /**
      * @hide
@@ -91,5 +94,24 @@ public class HashUtils {
         if(pm != null) {
             pm.goToSleep(SystemClock.uptimeMillis());
         }
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
     }
 }
