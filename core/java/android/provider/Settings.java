omnirom/android_frameworks_base_old

[1/2] base: Add toggle for face auto unlock
neobuddy89: Place boolean correctly.

Change-Id: I119fa9a4686d4ef1991c40c39225ddb5e12d25d9

core/java/android/provider/Settings.java
@@ -7847,6 +7847,13 @@ public final class Settings {
         public static final String LOCK_QS_DISABLED = "lockscreen_qs_disabled";
 
         /**
+         * Enable face auto unlock on secure lock screens
+         *
+         * @hide
+         */
+        public static final String FACE_AUTO_UNLOCK = "face_auto_unlock";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear

HashOS/android_frameworks_base

Change-Id: I907cf497553d75da87f8faab01553a716f2b3244

core/java/android/provider/Settings.java
@@ -8269,6 +8269,13 @@ public final class Settings {
         public static final String HASH_LOCK_QS_DISABLED = "lockscreen_qs_disabled";
 
         /**
+         * Enable face auto unlock on secure lock screens
+         *
+         * @hide
+         */
+        public static final String HASH_FACE_AUTO_UNLOCK = "face_auto_unlock";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear